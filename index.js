e818865a336f89e19630c740ada38328c6062025
Faiq Ali <faiqalii19@gmail.com>
2021-07-20 22:16:16 +0500

    callback practise

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const rectangle = require('./rectangle.js');
 let rect = require('./rectangle.js');
 
 let solverect = (l,b) => {
-    if(l<=0 || b<=0){
-        console.log("Lenght and Breadht should be greater than zero");
-    }
-    else{
-        console.log("Perimeter of a rectangle is " + rect.perimeter(l,b));
-        console.log("Area of a rectangle is " + rect.area(l,b));
-    }
+    
+    rect(l,b, (err , rectangle) => {
+        if(err){
+            console.log("ERROR: ", err.message);
+        }
+        else{
+            console.log("Perimeter of a rectangle is " + rectangle.perimeter(l,b));
+            console.log("Area of a rectangle is " + rectangle.area(l,b));
+        }
+    });
 }
 
 solverect(4,5);
+solverect(-2,3);
